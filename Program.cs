731f5df148748ca8afb5bbde52482e7b737754d1
Amritpal Kaur <amritpalkaur@amritpals-air.gateway.sasktel.net>
2020-07-14 16:46:09 -0600

    my changes

--- Program.cs
+++ Program.cs
@@ -35,6 +35,7 @@ namespace oopquiz
             myBike.SpeedUp();
 
             myBike.StopMoving();
+           
 
             Console.WriteLine($"My speed: {myBike.CurrentSpeed}");
             if (myBike.CurrentSpeed != 0) {
@@ -80,56 +81,71 @@ namespace oopquiz
         }
     }
 
-    class Vehicle {
-        // Some change...
+    //class Vehicle {
+        abstract class Vehicle{
         public int NumWheels { get; }
         public bool IsElectric { get; }
         protected int _maxPassengers;
-        bool _isMoving;
+       protected bool _isMoving;
 
         // Constructor
-        public Vehicle Vehicle(int numWheels, bool isElectric, int maxPassengers) {
+       // public Vehicle Vehicle(int numWheels, bool isElectric, int maxPassengers) { //amrit
+            public  Vehicle(int numWheels, bool isElectric, int maxPassengers) {
             NumWheels = numWheels;
             IsElectric = isElectric;
             _maxPassengers = maxPassengers;
         }
 
         // Regular method
-        public bool CanWeAllFit(int[] people) {
+       // public bool CanWeAllFit(int[] people) {
+             public bool CanWeAllFit(String[] people) {
             // Bonus challenge: Make this function into one line
-            if (people.Length <= _maxPassengers) {
-                return true;
-            } else {
-                return false;
+            // if (people.Length <= _maxPassengers) {
+            //     return true;
+            // } else {
+            //     return false;
+            // }
+            {
+               return people.Length <= _maxPassengers?true:false;
             }
         }
 
         // Virtual defaults
-        public virtual StartMoving() {
+       // public virtual StartMoving() {
+            public virtual void StartMoving() {
             _isMoving = true;
         }
 
-        public virtual override void StopMoving() {
+        //public virtual override void StopMoving() {
+            public virtual  void StopMoving() {
             _isMoving = false;
+           
         }
 
         // Abstract methods
-        abstract void SpeedUp();
-        abstract void SlowDown() {
-            _isMoving--;
-        }
+        //abstract void SpeedUp();
+        public abstract void SpeedUp();
+
+
+        //abstract void SlowDown() {
+           //  _isMoving--;
+           public abstract void SlowDown() ;
+           
+        
     }
 
-    class Bicycle {
+   // class Bicycle { //amrit
+        class Bicycle : Vehicle{
         public int NumSpeeds { get; }
         public string BikeType { get; }
         public int Year { get; }
-        public int CurrentSpeed { get; set; }
+        public  int CurrentSpeed { get; private set; } 
 
         // Keeps track of the total number of bikes initialized
-        int NumberOfBikes = 0;
+        //int NumberOfBikes = 0;
+        public static int NumberOfBikes = 0;
 
-        static List<string> _validBikeTypes = new List<string>() { "Road", "Hybrid" };
+        static List<string> _validBikeTypes = new List<string>() { "Road", "Hybrid", "Mountain" }; // add mountain
 
         public Bicycle(
             int numSpeeds, 
@@ -137,8 +153,6 @@ namespace oopquiz
             int year
         ) : base(2, false, 1) {
             if (_validBikeTypes.Contains(bikeType)) {
-                Console.WriteLine("Invalid bike type: {bikeType}");
-            }
             CurrentSpeed = 0;
             _isMoving = false;
             Year = year;
@@ -147,18 +161,41 @@ namespace oopquiz
 
             // Increment the total number of bikes
             NumberOfBikes++;
+            }
+            else throw new Exception($"Invalid bike type: {bikeType}");
+            // {
+            // Console.WriteLine("Invalid bike type: {bikeType}"); 
+            // }
+            
         }
 
-        public void SpeedUp() {
-            if (CurrentSpeed < NumSpeeds) {
-                CurrentSpeed++;
+        public override void SpeedUp() {
+            if (_isMoving && CurrentSpeed < NumSpeeds) {             
+                CurrentSpeed++;                    
             }
+           else if(_isMoving==false) throw new Exception("cant speed up without starting");
+          else if (CurrentSpeed==NumSpeeds) throw new Exception("Maximum Speed limit reached!");
         }
 
-        public void Slowdown() {
-            if (_isMoving && CurrentSpeed > 0) {
-                _currentSpeed--;
+        public override void SlowDown() {
+           
+            if (_isMoving && CurrentSpeed > 1) {
+                //_currentSpeed--;           
+                CurrentSpeed--; 
             }
+        }
+        public override void StartMoving() {
+            _isMoving = true;
+            CurrentSpeed++;
+           
+        }
+
+        //public virtual override void StopMoving() {
+            public override  void StopMoving() {
+            _isMoving = false;
+            CurrentSpeed=0;
+
+           
         }
     }
 }
